fd43e5aa2839a94cf8b83c555137e928103cdd79
Sandesh <sandeshbansal24@gmail.com>
2021-03-05 19:55:47 +0530

    strings

---
+++ sandesh/strings.py
@@ -0,0 +1,12 @@
+#string in array
+a = "Hello Everyone, Hope you all are fine!"
+print(a[8])
+
+#length of a string
+print(len(a))
+
+#checking a word in a string
+print("are" in a)
+if "fine" in a:
+    print("True")
+    
